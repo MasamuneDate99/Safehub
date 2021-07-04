424337b557cf6eec69fe7011628139de78b9adf6
TYR <natdavin@gmail.com>
2021-07-04 16:11:33 +0700

    Loading Splash Screen done

---
+++ app/src/main/java/united/huawei/safehub/MainMenu.java
@@ -0,0 +1,14 @@
+package united.huawei.safehub;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}
