d7daf79150ef7c9e538c001e26247ebb3adf1b62
TYR <natdavin@gmail.com>
2021-07-12 17:02:45 +0700

    Done rapihin isi Daily Case + Daily Case preview tanpa button

--- app/src/main/java/united/huawei/safehub/DailyCase.java
+++ app/src/main/java/united/huawei/safehub/DailyCase.java
@@ -1,8 +1,6 @@
 package united.huawei.safehub;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,29 +11,27 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import androidx.annotation.Nullable;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DailyCase extends AppCompatActivity {
     private TextView dataCovid;
+    private TextView titleMain;
+    private TextView titleDaily;
+    private TextView dataCovidB;
     private RequestQueue dataRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.daily_case);
 
-        dataCovid = findViewById(R.id.DataHarianCovid);
-        Button btnParse = findViewById(R.id.RequestData);
+        dataCovidB = findViewById(R.id.DataHarianCovid);
+        dataCovid = findViewById(R.id.DataSemua);
+        titleMain = findViewById(R.id.MainTitle);
+        titleDaily = findViewById(R.id.DailyTitle);
         dataRequest = Volley.newRequestQueue(this);
-        btnParse.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                jsonParse();
-            }
-        });
+        jsonParse();
     }
 
     private void jsonParse() {
@@ -45,29 +41,28 @@ public class DailyCase extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            //JSONArray jsonArray = response.getJSONArray("data");
-                            //for (int i = 0;i < jsonArray.length(); i++) {
-                                JSONObject dataHarian = response.getJSONObject("data");
-                                JSONObject dataTotal = response.getJSONObject("update").getJSONObject("penambahan");
+                            JSONObject dataHarian = response.getJSONObject("data");
+                            JSONObject dataTotal = response.getJSONObject("update").getJSONObject("penambahan");
 
-                                int Odp = dataHarian.getInt("id");
-                                int Pdp = dataHarian.getInt("jumlah_odp");
+                            int Odp = dataHarian.getInt("id");
+                            int Pdp = dataHarian.getInt("jumlah_odp");
 
-                                int Positif = dataTotal.getInt("jumlah_positif");
-                                int Meninggal = dataTotal.getInt("jumlah_meninggal");
-                                int Sembuh = dataTotal.getInt("jumlah_sembuh");
-                                int Dirawat = dataTotal.getInt("jumlah_dirawat");
-                                String TanggalKasus = dataTotal.getString("tanggal");
+                            int Positif = dataTotal.getInt("jumlah_positif");
+                            int Meninggal = dataTotal.getInt("jumlah_meninggal");
+                            int Sembuh = dataTotal.getInt("jumlah_sembuh");
+                            int Dirawat = dataTotal.getInt("jumlah_dirawat");
+                            String TanggalKasus = dataTotal.getString("tanggal");
 
-                                dataCovid.append("COVID-19 Data" + "\n"
-                                + "Jumlah ODP : " + String.valueOf(Odp) + "\n"
-                                + "Jumlah PDP : " + String.valueOf(Pdp) + "\n"
-                                + "Jumlah Positif : " + String.valueOf(Positif) + "\n"
-                                + "Jumlah Meninggal : " + String.valueOf(Meninggal) + "\n"
-                                + "Jumlah Sembuh" + String.valueOf(Sembuh) + "\n"
-                                + "Jumlah Dirawat" + String.valueOf(Dirawat) + "\n"
-                                + "Tanggal Kasus Harian" + TanggalKasus + "\n\n");
-                            //}
+                            dataCovid.append("Jumlah ODP : " + String.valueOf(Odp) + "\n"
+                                + "Jumlah PDP : " + String.valueOf(Pdp) + "\n");
+
+                            dataCovidB.append("Jumlah Positif : " + String.valueOf(Positif) + "\n"
+                                    + "Jumlah Meninggal : " + String.valueOf(Meninggal) + "\n"
+                                    + "Jumlah Sembuh" + String.valueOf(Sembuh) + "\n"
+                                    + "Jumlah Dirawat" + String.valueOf(Dirawat) + "\n"
+                                    + "Tanggal Kasus Harian" + TanggalKasus + "\n\n");
+                            titleMain.append("Indonesian COVID-19 Information");
+                            titleDaily.append("Daily Case");
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
