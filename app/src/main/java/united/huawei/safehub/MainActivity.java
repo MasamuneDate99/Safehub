6b5b7c8b735dd16e6b54d04f8e7fb86e3f289658
TYR <natdavin@gmail.com>
2021-07-16 12:04:49 +0700

    Temporary Move JSON Parsing to MainMenu ( di comment dulu karena Activity Dailynya blom di pindahin )

--- app/src/main/java/united/huawei/safehub/MainActivity.java
+++ app/src/main/java/united/huawei/safehub/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
