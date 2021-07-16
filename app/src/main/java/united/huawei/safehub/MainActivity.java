4345a04a6b0b1a62a9cb6fd437be9ce7c13e8395
TYR <natdavin@gmail.com>
2021-07-16 12:04:29 +0700

    Temporary Move JSON Parsing to MainMenu ( di comment dulu karena Activity Dailynya blom di pindahin )

--- app/src/main/java/united/huawei/safehub/MainActivity.java
+++ app/src/main/java/united/huawei/safehub/MainActivity.java
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/united/huawei/safehub/MainMenu.java
+++ app/src/main/java/united/huawei/safehub/MainMenu.java
@@ -5,15 +5,26 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-public class MainMenu extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainMenu extends AppCompatActivity {
+    private TextView dataCovid;
+    private TextView titleMain;
+    private TextView titleDaily;
+    private TextView dataCovidB;
+    private RequestQueue dataRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +34,50 @@ public class MainMenu extends AppCompatActivity {
         Button privacyBtn = findViewById(R.id.privacyPolicy);
         Button dailyBtn = findViewById(R.id.dailyCase);
 
+        //dataCovidB = findViewById(R.id.DataHarianCovid);
+        //dataCovid = findViewById(R.id.DataSemua);
+        //titleMain = findViewById(R.id.MainTitle);
+        //titleDaily = findViewById(R.id.DailyTitle);
+        //dataRequest = Volley.newRequestQueue(this);
+        //jsonParse();
+
         mapbtn.setOnClickListener(v -> startActivity(new Intent(MainMenu.this, MapsActivity.class)));
         privacyBtn.setOnClickListener(v -> startActivity(new Intent(MainMenu.this, PrivacyPolicy.class)));
         dailyBtn.setOnClickListener(v -> startActivity(new Intent(MainMenu.this, DailyCase.class)));
         permission();
+    }
+    private void jsonParse() {
+        String dataSource = "https://data.covid19.go.id/public/api/update.json";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, dataSource, null,
+                response -> {
+                    try {
+                        JSONObject dataHarian = response.getJSONObject("data");
+                        JSONObject dataTotal = response.getJSONObject("update").getJSONObject("penambahan");
+
+                        int Odp = dataHarian.getInt("jumlah_odp");
+                        int Pdp = dataHarian.getInt("jumlah_pdp");
+
+                        int Positif = dataTotal.getInt("jumlah_positif");
+                        int Meninggal = dataTotal.getInt("jumlah_meninggal");
+                        int Sembuh = dataTotal.getInt("jumlah_sembuh");
+                        int Dirawat = dataTotal.getInt("jumlah_dirawat");
+                        String TanggalKasus = dataTotal.getString("tanggal");
+
+                        dataCovid.append("Jumlah ODP : " + Odp + "\n"
+                                + "Jumlah PDP : " + Pdp + "\n");
+
+                        dataCovidB.append("Jumlah Positif : " + Positif + "\n"
+                                + "Jumlah Meninggal : " + Meninggal + "\n"
+                                + "Jumlah Sembuh : " + Sembuh + "\n"
+                                + "Jumlah Dirawat : " + Dirawat + "\n"
+                                + "Tanggal Kasus Harian : " + TanggalKasus + "\n\n");
+                        titleMain.append("Indonesian COVID-19 Information");
+                        titleDaily.append("Daily Case");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }, Throwable::printStackTrace);
+        dataRequest.add(request);
     }
 
     void permission(){
@@ -53,10 +104,6 @@ public class MainMenu extends AppCompatActivity {
                         Manifest.permission.READ_EXTERNAL_STORAGE};
                 ActivityCompat.requestPermissions(this, strings, 2);
             }
-
         }
-
-
-
     }
 }
